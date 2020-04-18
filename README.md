841703ea4ad7fe546ce0c2e54ec25574135dfd66
Jonathan <jonathan213kim@gmail.com>
2020-04-17 21:26:48 -0700

    cleaned up comments

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # 03RandomPWGenerator
+
+## Credit
+
+https://www.w3schools.com/jsref/jsref_push.asp
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
-
 var password = [];
-
 var answerIndex = [];
 var lowerAlpha = [
   "a",
@@ -32,16 +29,13 @@ var lowerAlpha = [
   "y",
   "z"
 ];
-var integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialChars = ["!", "#", "$", "%", "&", "?", "@"];
-
 function uppercase(lowercase) {
   return lowercase.toUpperCase();
 }
-
 var upperAlpha = lowerAlpha.map(uppercase);
+var integers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var specialChars = ["!", "#", "$", "%", "&", "?", "@"];
 
-// Write password to the #password input
 function writePassword() {
   var passwordCriteria = {
     charLength: prompt(
@@ -62,23 +56,18 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
   if (passwordCriteria.charLength >= 8 && passwordCriteria.charLength <= 128) {
     password.length = passwordCriteria.charLength;
-    // Check Lowercase
     if (passwordCriteria.charLowercase) {
       Array.prototype.push.apply(answerIndex, lowerAlpha);
     }
-    // Check Uppercase
     if (passwordCriteria.charUppercase) {
       Array.prototype.push.apply(answerIndex, upperAlpha);
     }
-    // Check numbers
     if (passwordCriteria.charNumeric) {
       Array.prototype.push.apply(answerIndex, integers);
     }
-    // Check "/"
     if (passwordCriteria.charSlash) {
       answerIndex.push("/");
     }
-    // Check special characters
     if (passwordCriteria.charSpecial) {
       Array.prototype.push.apply(answerIndex, specialChars);
     }
@@ -86,18 +75,12 @@ function writePassword() {
     alert("Please input a correct number");
   }
 
-  ///////////////////////////////////////////////
   var i;
   for (i = 0; i < passwordCriteria.charLength; i++) {
     password.push(answerIndex[Math.floor(Math.random() * answerIndex.length)]);
     var passwordFinal = password.toString().replace(/,/g, "");
-    console.log(passwordFinal);
   }
-  // passwordText.value = password.toString().replace(/,/g, "");
   passwordText.value = passwordFinal;
-
-  // To generate the password.
-  // function generatePassword() {}
 }
-// Add event listener to generate button
+
 generateBtn.addEventListener("click", writePassword);
